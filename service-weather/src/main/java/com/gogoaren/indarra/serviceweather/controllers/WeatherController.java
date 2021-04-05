749d5ed7239be40e6723e149f0957c5f40657e90
gogoarenindarra <jablonski.pio@gmail.com>
2021-04-05 21:31:02 +0200

    fix h2

--- service-weather/src/main/java/com/gogoaren/indarra/serviceweather/controllers/WeatherController.java
+++ service-weather/src/main/java/com/gogoaren/indarra/serviceweather/controllers/WeatherController.java
@@ -6,10 +6,18 @@ import com.gogoaren.indarra.serviceweather.data.weather.WeatherEntityService;
 import com.gogoaren.indarra.serviceweather.fetch.OpenWeatherFetcher;
 import com.gogoaren.indarra.serviceweather.fetch.OpenWeatherResponse;
 import com.gogoaren.indarra.serviceweather.fetch.OpenWeatherResponseConverter;
+import com.gogoaren.indarra.serviceweather.fetch.Weather;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
 
 @RequestMapping(produces = {MediaType.APPLICATION_JSON_VALUE}, path = "/api/weather")
 @RestController
@@ -22,14 +30,42 @@ public class WeatherController {
     private WeatherEntityService weatherEntityService;
 
     @GetMapping(value = "/{city}")
-    public WeatherEntity getWeatherByCity(@PathVariable String city) {
+    public Weather getWeatherByCity(@PathVariable String city) {
+
+        Optional<WeatherEntity> optionalWeatherEntity =
+                weatherEntityService.findLatestStoredTemperature(city);
+
+
+        if (optionalWeatherEntity.isPresent()) {
+            log.info("weather from db: " + optionalWeatherEntity);
+
+            WeatherEntity weatherEntity = optionalWeatherEntity.get();
+//            var weatherEntity = optionalWeatherEntity.get();
+            return Weather.builder()
+                    .humidity(weatherEntity.getHumidity())
+                    .city(weatherEntity.getCity())
+                    .temperature(weatherEntity.getTemperature())
+                    .wind(weatherEntity.getWind())
+                    .build();
+        }
+
+
         log.info("fetching weather for: " + city);
         OpenWeatherResponse openWeatherResponse = openWeatherFetcher.fetchWeatherByCityName(city);
         log.info("open weather response: " + openWeatherResponse);
+        Weather weather = converter.convert(openWeatherResponse);
 
-        WeatherEntity weatherEntity = converter.convert(openWeatherResponse);
+        WeatherEntity weatherEntity = WeatherEntity.builder()
+                .uuid(UUID.randomUUID())
+                .created(Instant.now())
+                .city(weather.getCity())
+                .humidity(weather.getHumidity())
+                .temperature(weather.getTemperature())
+                .build();
+        log.info("save weather entity: " + weatherEntity.toString());
         weatherEntityService.saveEntity(weatherEntity);
-        return weatherEntity;
+        return weather;
     }
+
 
 }
--- service-weather/src/main/java/com/gogoaren/indarra/serviceweather/data/weather/WeatherEntity.java
+++ service-weather/src/main/java/com/gogoaren/indarra/serviceweather/data/weather/WeatherEntity.java
@@ -3,31 +3,32 @@ package com.gogoaren.indarra.serviceweather.data.weather;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import org.hibernate.annotations.DynamicInsert;
-import org.hibernate.annotations.DynamicUpdate;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.UUID;
 
 @Data
 @Builder
-//@MappedSuperclass
-//@Access(value = AccessType.FIELD)
-//@DynamicInsert
-//@DynamicUpdate
 @Table(name = "weather")
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class WeatherEntity implements Serializable {
 
     private static final long serialVersionUID = 345L;
 
     @Id
     private UUID uuid;
-    private String city;
     private Instant created;
-    private Float temperature;
-    private Float humidity;
+    private String city;
+    private double temperature;
+    private double humidity;
+    private double wind;
+
 
 }
