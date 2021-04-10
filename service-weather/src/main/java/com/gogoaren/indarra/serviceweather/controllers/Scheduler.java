71128de528ee6d4f2f349ce6fe9e6f1cd5e2614d
gogoarenindarra <jablonski.pio@gmail.com>
2021-04-10 10:38:33 +0200

    fix scheduler, add statistic service

--- service-weather/src/main/java/com/gogoaren/indarra/serviceweather/controllers/Scheduler.java
+++ service-weather/src/main/java/com/gogoaren/indarra/serviceweather/controllers/Scheduler.java
@@ -2,6 +2,7 @@ package com.gogoaren.indarra.serviceweather.controllers;
 
 import com.gogoaren.indarra.serviceweather.fetch.WeatherService;
 import com.gogoaren.indarra.serviceweather.utils.CountryCodeService;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,40 +12,35 @@ import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
 
 @Component
 @Slf4j
+@AllArgsConstructor
 public class Scheduler {
-//
-//    @Autowired
-//    WeatherService weatherService;
-//
-//    @Scheduled(fixedDelayString = "${weather.schedule.update.ms}")
-//    public void uploadWeatherScheduleTask() {
-//
-//        List<String> cityNamesB = Arrays.asList("London", "Warsaw", "Berlin",
-//                "Alicante", "Albuquerque", "Moscow", "Jijona", "Novomoskovsk", "Gebze", "Briançon");
-//        Random rand = new Random();
-//        String randomElement = cityNamesB.get(rand.nextInt(cityNamesB.size()));
-//        var weather = weatherService.getWeatherByCity(randomElement);
-//        log.info("Weather for " + randomElement + " at time: " + Instant.now() + " weather:  " + weather);
-//        log.info(weather.getCountry() + " - coverted country name " + CountryCodeService.iso2CountryCodeToCountryName(weather.getCountry()));
-//    }
+
+    @Autowired
+    WeatherService weatherService;
+
+    @Scheduled(fixedDelayString = "${weather.schedule.update.ms}")
+    public void uploadWeatherScheduleTask() {
+
+        List<String> cityNamesB = Arrays.asList("London", "Warsaw", "Berlin",
+                "Alicante", "Albuquerque", "Moscow", "Jijona", "Novomoskovsk", "Gebze", "Briançon");
+        Random rand = new Random();
+        String randomElement = cityNamesB.get(rand.nextInt(cityNamesB.size()));
+        var weather = weatherService.getWeatherByCity(randomElement);
+        log.info("Weather for " + randomElement + " at time: " + Instant.now() + " weather:  " + weather);
+        log.info(weather.getCountry() + " - coverted country name " + CountryCodeService.iso2CountryCodeToCountryName(weather.getCountry()));
+    }
 
 
-//    @Scheduled(cron = "${weather.schedule.cron.london}")
-//    public void uploadLondonWeatherScheduleTask() {
-//        uploadWeather("London");
-//    }
-//
-//    private void uploadWeather(String city) {
-//        var weather = weatherService.getWeatherByCity(city);
-//        log.info("Weather for " + city + " at time: " + Instant.now() + " weather:  " + weather);
-//    }
+    @Scheduled(cron = "${weather.schedule.cron.london}")
+    public void uploadLondonWeatherScheduleTask() {
+        uploadWeather("London");
+    }
+
+    private void uploadWeather(String city) {
+        var weather = weatherService.getWeatherByCity(city);
+        log.info("Weather for " + city + " at time: " + Instant.now() + " weather:  " + weather);
+    }
 }
-
-/*
-* "Albuquerque"
-* */
